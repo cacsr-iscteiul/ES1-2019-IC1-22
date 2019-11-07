66308b91ef55162c80d179ab276a0469946d91b6
Laura Pereira <lvpas@iscte-iul.pt>
2019-11-07 13:28:22 +0000

    sem nada escrito

--- Projeto/src/Teste.java
+++ Projeto/src/Teste.java
@@ -1,13 +1,4 @@
 
 public class Teste {
-//isto e teste
-	//fuck you bitch
-	
-//VOCES SAO OS MAIORES MEUS BOYZZZ
-	//yoyo como e q e? ta tdo em ordem?
-	//OLA SOU A MARTXICAS
-	//ta tudo em ordem?
-	
-	// Quando quiseres trrabalhar e parar de fingires que tens erros diz ANDRE
-	//ola tod bem
+
 }
