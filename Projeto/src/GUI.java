e3539434523f52175f1b29c3c9d1c571051a8b29
cacsr-iscteiul <cacsr@iscte-iul.pt>
2019-11-11 10:25:42 +0000

    GUI2

--- Projeto/src/GUI.java
+++ Projeto/src/GUI.java
@@ -2,6 +2,14 @@ import javax.swing.JFrame;
 
 public class GUI {
 
+	public GUI(String[] args) {
+		
+	}
+	
 	JFrame janela = new JFrame("GUI");
+	
+	public static void main(String[] args) {
+		GUI gui = new GUI(args);
+	}
 	
 }
