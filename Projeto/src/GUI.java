2669e1a13b4466a4dd5351377503df7a05fca4c7
cacsr-iscteiul <cacsr@iscte-iul.pt>
2019-11-11 10:27:53 +0000

    Erro

--- Projeto/src/GUI.java
+++ Projeto/src/GUI.java
@@ -2,15 +2,15 @@ import javax.swing.JFrame;
 
 public class GUI {
 
-	public GUI(String[] args) {
-		
-	}
+//	public GUI(String[] args) {
+//		
+//	}
 	
 	JFrame janela = new JFrame("GUI");
 	
-	public static void main(String[] args) {
-		GUI gui = new GUI(args);
-	}
+//	public static void main(String[] args) {
+//		GUI gui = new GUI(args);
+//	}
 	
 	//erro
 	
