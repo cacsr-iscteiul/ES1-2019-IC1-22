3c574670ada835048bac94184236029f0d2e501c
marma-iscteiul <marma@iscte-iul.pt>
2019-12-04 14:05:30 +0000

    Proj

--- ProjetoES/src/Projeto/App.java
+++ ProjetoES/src/Projeto/App.java
@@ -2,6 +2,8 @@ package ProjetoES;
 
 import java.awt.Font;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,35 +12,54 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 public class App {
     
 
 	
-	public static void main(String[] args) {
-    	
-  File excel = new File("Long-Method");
-  Workbook worb = new XSSFWorkbook(excel);
-  XSSFSheet sheet = worb.getSheetAt(0);
-  
-  Iterator<Row> rowi = sheet.iterator();
-  
-  while(row.hasNext()) {
-	  Row row = rowi.next()
-			  Iterator<Cell> celli = row.cellIterator()
-			  
-			  while(celli.hasNext()) {
-				  Cell cell = celli.next();
-				  System.out.println(cell.toString() + ";");
-						  
-			  }
-	  System.out.println();
-  }
-		
-		worb.close();
-		excel.close();
-		
-	}
+	public static void main(String[] args) throws IOException {
+   	 try
+        {
+            FileInputStream file = new FileInputStream(new File("Long-Method.xlsx"));
+ 
+            //Create Workbook instance holding reference to .xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+ 
+            //Get first/desired sheet from the workbook
+            XSSFSheet sheet = workbook.getSheetAt(0);
+ 
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) 
+            {
+                Row row = rowIterator.next();
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+                 
+                while (cellIterator.hasNext()) 
+                {
+                    Cell cell = cellIterator.next();
+                    //Check the cell type and format accordingly
+                    switch (cell.getCellType()) 
+                    {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "t");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "t");
+                            break;
+                    }
+                }
+                System.out.println(workbook.getNumberOfSheets());
+            }
+            file.close();
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
 
 }
