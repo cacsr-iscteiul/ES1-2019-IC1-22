36bf80ae680ba835f6a6c2c91122cd0189641e2a
mpsci-iscteiul <mpsci@iscte-iul.pt>
2019-12-10 03:24:54 +0000

    mekie

--- ProjetoES22/src/main/java/ProjetoES/GUI.java
+++ ProjetoES22/src/main/java/ProjetoES/GUI.java
@@ -45,6 +45,7 @@ public class GUI {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTable table;
+	DefaultTableModel dtm = new DefaultTableModel();  //mudei isto para aqui
 
 	/**
 	 * Launch the application.
@@ -77,20 +78,20 @@ public class GUI {
 		frame.setBounds(100, 100, 894, 567);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblQualidadeDeDeteo = new JLabel("Qualidade de Deteção de Erros");
 		lblQualidadeDeDeteo.setBounds(99, 11, 200, 14);
 		lblQualidadeDeDeteo.setForeground(Color.BLUE);
 		frame.getContentPane().add(lblQualidadeDeDeteo);
 
-		
+
 		JButton btnMostrarExcel = new JButton("Mostrar Excel");
 		btnMostrarExcel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Excel ex = new Excel();
 				final JFileChooser jf = new JFileChooser();
 				int jff = jf.showOpenDialog(jf);
-				DefaultTableModel dtm = new DefaultTableModel();
+			//	DefaultTableModel dtm = new DefaultTableModel();
 				if(jff ==JFileChooser.APPROVE_OPTION) {
 					try { 
 						File excel = jf.getSelectedFile();
@@ -98,7 +99,7 @@ public class GUI {
 						BufferedInputStream excelBIS = new BufferedInputStream(excelFIS);
 						XSSFWorkbook excelJTable = new XSSFWorkbook(excelBIS);
 						XSSFSheet excelsh = excelJTable.getSheetAt(0);
-						
+
 						boolean primlinha = true;
 						for(int coluna=0; coluna <= excelsh.getLastRowNum(); coluna++) {
 							XSSFRow colunaexcel = excelsh.getRow(coluna); 
@@ -111,15 +112,16 @@ public class GUI {
 								primlinha=true;
 							}else
 								dtm.addRow(cell);
-								
+					//		System.out.println(cell);
+
 						}
-								
-							
-						}catch (FileNotFoundException e) {
-							JOptionPane.showMessageDialog(null, e.getMessage());
-						} catch (IOException e) {
-							JOptionPane.showMessageDialog(null, e.getMessage());
-						} finally {
+
+
+					}catch (FileNotFoundException e) {
+						JOptionPane.showMessageDialog(null, e.getMessage());
+					} catch (IOException e) {
+						JOptionPane.showMessageDialog(null, e.getMessage());
+					} finally {
 						try {
 							ex.readexcel();
 						} catch (IOException e) {
@@ -127,58 +129,68 @@ public class GUI {
 							e.printStackTrace();
 						}
 					}
-					
+
 				}}
 		});
 		btnMostrarExcel.setBounds(109, 50, 145, 23);
 		frame.getContentPane().add(btnMostrarExcel);
-		
+
 		JComboBox Metrica = new JComboBox();
 		Metrica.setBounds(26, 113, 74, 20);
 		Metrica.setModel(new DefaultComboBoxModel(new String[] {"Metrica", "LOC", "CYCLO", "ATFD", "LAA"}));
 		Metrica.setToolTipText("");
 		frame.getContentPane().add(Metrica);
-		
+
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(131, 113, 104, 20);
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Comparador", ">", "<", ">=", "<="}));
 		frame.getContentPane().add(comboBox);
-		
+
 		textField = new JTextField();
 		textField.setBounds(285, 113, 86, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(131, 196, 104, 20);
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"And", "Or"}));
 		frame.getContentPane().add(comboBox_1);
-		 
+
 		JComboBox comboBox_2 = new JComboBox();
 		comboBox_2.setBounds(26, 280, 74, 20);
 		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Metrica", "LOC", "CYCLO", "ATFD", "LAA"}));
 		frame.getContentPane().add(comboBox_2);
-		 
+
 		JComboBox comboBox_3 = new JComboBox();
 		comboBox_3.setBounds(131, 280, 104, 20);
 		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"Comparador", ">", "<", ">=", "<="}));
 		frame.getContentPane().add(comboBox_3);
-		 
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(285, 280, 86, 20);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		btnAvaliarQualidasw.setBounds(109, 375, 145, 23);
 		frame.getContentPane().add(btnAvaliarQualidasw);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(395, 11, 473, 506);
 		frame.getContentPane().add(scrollPane);
-		
+
 		table = new JTable();
-		
+
 		scrollPane.setViewportView(table);
-		
-		
+
+//		table.setModel(new DefaultTableModel(
+//				new Object[][] {
+//					
+//				},
+//				new String [] {
+//						
+//				}
+//				));
+		table.setModel(dtm); // fez me sentido ser assim
+
+
 	}
 }
