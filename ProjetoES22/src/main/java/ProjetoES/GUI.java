65b9e9fc6d91803c8925d479bedc602afbcb9be7
marma-iscteiul <marma@iscte-iul.pt>
2019-12-10 00:08:24 +0000

    ffff

--- ProjetoES22/src/main/java/ProjetoES/GUI.java
+++ ProjetoES22/src/main/java/ProjetoES/GUI.java
@@ -7,12 +7,19 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.Color;
@@ -24,6 +31,12 @@ import java.awt.event.ContainerAdapter;
 import java.awt.event.ContainerEvent;
 import java.awt.event.InputMethodListener;
 import java.awt.event.InputMethodEvent;
+import javax.swing.table.DefaultTableModel;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class GUI {
 
@@ -61,37 +74,68 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 726, 490);
+		frame.setBounds(100, 100, 776, 567);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblQualidadeDeDeteo = new JLabel("Qualidade de Deteção de Erros");
-		lblQualidadeDeDeteo.setBounds(86, 11, 200, 14);
+		lblQualidadeDeDeteo.setBounds(99, 11, 200, 14);
 		lblQualidadeDeDeteo.setForeground(Color.BLUE);
 		frame.getContentPane().add(lblQualidadeDeDeteo);
 
 		
 		JButton btnMostrarExcel = new JButton("Mostrar Excel");
-		btnMostrarExcel.setBounds(109, 50, 134, 23);
 		btnMostrarExcel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				try {
-					Excel excel = new Excel();
-					excel.readexcel();
+				Excel ex = new Excel();
+				final JFileChooser jf = new JFileChooser();
+				int jff = jf.showOpenDialog(jf);
+				DefaultTableModel dtm = new DefaultTableModel();
+				if(jff ==JFileChooser.APPROVE_OPTION) {
+					try {
+						File excel = jf.getSelectedFile();
+						FileInputStream excelFIS = new FileInputStream(excel);
+						BufferedInputStream excelBIS = new BufferedInputStream(excelFIS);
+						XSSFWorkbook excelJTable = new XSSFWorkbook(excelBIS);
+						XSSFSheet excelsh = excelJTable.getSheetAt(0);
+						
+						boolean primlinha = true;
+						for(int coluna=0; coluna <= excelsh.getLastRowNum(); coluna++) {
+							XSSFRow colunaexcel = excelsh.getRow(coluna); 
+							XSSFCell[] cell = new XSSFCell[colunaexcel.getLastCellNum()];
+							for(int a = 0; a<colunaexcel.getLastCellNum(); a++) {
+								cell[a] = colunaexcel.getCell(a);
+							}
+							if(primlinha) {
+								dtm.setColumnIdentifiers(cell);
+								primlinha=true;
+							}else
+								dtm.addRow(cell);
+								
+						}
+								
+							
+						}catch (FileNotFoundException e) {
+							JOptionPane.showMessageDialog(null, e.getMessage());
+						} catch (IOException e) {
+							JOptionPane.showMessageDialog(null, e.getMessage());
+						} finally {
+						try {
+							ex.readexcel();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
 					
-					 
-			} catch (IOException e1) {
-					// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		
-		}
+				}}
 		});
+		btnMostrarExcel.setBounds(109, 50, 145, 23);
 		frame.getContentPane().add(btnMostrarExcel);
 		
 		JComboBox Metrica = new JComboBox();
 		Metrica.setBounds(26, 113, 74, 20);
-		Metrica.setModel(new DefaultComboBoxModel(new String[] {"Metrica"}));
+		Metrica.setModel(new DefaultComboBoxModel(new String[] {"Metrica", "LOC", "CYCLO", "ATFD", "LAA"}));
 		Metrica.setToolTipText("");
 		frame.getContentPane().add(Metrica);
 		
@@ -106,13 +150,13 @@ public class GUI {
 		textField.setColumns(10);
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(131, 182, 104, 20);
+		comboBox_1.setBounds(131, 196, 104, 20);
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"And", "Or"}));
 		frame.getContentPane().add(comboBox_1);
 		 
 		JComboBox comboBox_2 = new JComboBox();
 		comboBox_2.setBounds(26, 280, 74, 20);
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Metrica"}));
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Metrica", "LOC", "CYCLO", "ATFD", "LAA"}));
 		frame.getContentPane().add(comboBox_2);
 		 
 		JComboBox comboBox_3 = new JComboBox();
@@ -124,13 +168,16 @@ public class GUI {
 		textField_1.setBounds(285, 280, 86, 20);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
-		btnAvaliarQualidasw.setBounds(118, 375, 117, 23);
+		btnAvaliarQualidasw.setBounds(109, 375, 145, 23);
 		frame.getContentPane().add(btnAvaliarQualidasw);
 		
-		table = new JTable();
-		table.setBounds(420, 50, 280, 390);
-		frame.getContentPane().add(table);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(395, 11, 355, 506);
+		frame.getContentPane().add(scrollPane);
 		
+		table = new JTable();
+		
+		scrollPane.setViewportView(table);
 		
 		
 	}
---
+++ ProjetoES22/src/main/java/ProjetoES/Metrica.java
@@ -0,0 +1,5 @@
+package ProjetoES;
+
+public enum Metrica {
+
+}
